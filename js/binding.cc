60efe0ca660afae8726a8facbec93f1b2e7539b1
Felipe <fm3209742@gmail.com>
2025-01-19 15:07:29 -0300

    ref: cleanup

--- js/binding.cc
+++ js/binding.cc
@@ -3,28 +3,25 @@
 #include <iostream>
 
 extern "C" {
-    const char* HelloGo(); // Declara a função Go que será chamada
+    const char* HelloGo();
 }
 
 Napi::String HelloGoWrapper(const Napi::CallbackInfo& info) {
-    // Tenta carregar a biblioteca Go
     void* handle = dlopen("./libhello.so", RTLD_LAZY);
     if (!handle) {
-        std::cerr << "Erro ao carregar a biblioteca Go: " << dlerror() << std::endl;
+        std::cerr << "Erro ao carregar a biblioteca Go: " << dlerror() << '\n';
         return Napi::String::New(info.Env(), "Erro ao carregar a biblioteca Go");
     }
 
-    // Chama a função Go depois de carregar a biblioteca
-    const char* result = HelloGo();  // Chama a função Go
-    dlclose(handle);  // Fecha a biblioteca
+    const char* result = HelloGo();  
+    dlclose(handle);
 
-    return Napi::String::New(info.Env(), result);  // Retorna o resultado para Node.js
+    return Napi::String::New(info.Env(), result);
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    // Registra a função HelloGoWrapper em Node.js
     exports.Set("helloGo", Napi::Function::New(env, HelloGoWrapper));
     return exports;
 }
 
-NODE_API_MODULE(libhello, Init) 
+NODE_API_MODULE(binding, Init) 
