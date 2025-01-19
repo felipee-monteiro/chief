5107a24b05d276449104bcbbc5c36de608965a8e
Felipe <fm3209742@gmail.com>
2025-01-19 15:59:24 -0300

    newbie: add bindings from cpp to nodejs

---
+++ binding.cc
@@ -0,0 +1,22 @@
+#include <napi.h>
+
+Napi::Value Add(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+
+    if (info.Length() < 2 || !info[0].IsNumber() || !info[1].IsNumber()) {
+        Napi::TypeError::New(env, "Dois números esperados").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    double arg0 = info[0].As<Napi::Number>().DoubleValue();
+    double arg1 = info[1].As<Napi::Number>().DoubleValue();
+
+    return Napi::Number::New(env, arg0 + arg1);
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+    exports.Set(Napi::String::New(env, "add"), Napi::Function::New(env, Add));
+    return exports;
+}
+
+NODE_API_MODULE(addon, Init)
--- js/index.js
+++
@@ -1,4 +0,0 @@
-const binding = require("./build/hello");
-
-
-
