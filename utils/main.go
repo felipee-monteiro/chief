45772d4a8de8a3bdcc7a66d9410f8e1464c64eee
void <felipe.antinho@sudema.pb.gov.br>
2025-03-16 20:48:31 +0000

    utils: check if a string is actually valid

--- utils/main.go
+++ utils/main.go
@@ -1,11 +1,20 @@
 package utils
 
 import (
-    "strconv"
+	"strconv"
+	"strings"
 )
 
 func IsNumeric(v string) bool {
-    _, err := strconv.Atoi(v)
+	_, err := strconv.Atoi(v)
 
-    return err == nil
+	return err == nil
+}
+
+func IsValidString(v string) bool {
+	if len(strings.TrimSpace(v)) == 0 {
+		return false
+	}
+
+	return true
 }
